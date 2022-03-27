56b44be42bb1f0cda58899abee9430f45e396145
Ashim Mahara <katyansun@gmail.com>
2022-03-27 09:24:14 +0545

    Cleaned up the misc code a bit

--- utils/misc.py
+++ utils/misc.py
@@ -1,13 +1,16 @@
 from torchinfo import summary
 
 import torch
+import torch.nn as nn
+
 from torch.utils.tensorboard import SummaryWriter
+from torch.utils.data.dataloader import DataLoader
 
 from datetime import datetime
 
 import os
 
-def get_summary(model: torch.nn.Module, dataloader = None, data = None) -> str:
+def get_summary(model: nn.Module, dataloader: DataLoader = None, data = None) -> str:
     
     """
     A very very dirty way to get model summary. TODO: Clean up a bit.
@@ -15,17 +18,15 @@ def get_summary(model: torch.nn.Module, dataloader = None, data = None) -> str:
     
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     
-    for idx, batch in enumerate(dataloader):
+    batch = next(iter(dataloader))
+    
+    sentences = batch['sentences'].to(device)
+    sentence_lengths = batch['sentence_lengths'].to(device, dtype= torch.int32)
 
-        sentences = batch['sentences'].to(device)
-        sentence_lengths = batch['sentence_lengths'].to(device, dtype= torch.int32)
+    melspecs = batch['melspecs'].to(device)
+    melspecs_lengths = batch['melspecs_lengths'].to(device, dtype= torch.int32)
 
-        melspecs = batch['melspecs'].to(device)
-        melspecs_lengths = batch['melspecs_lengths'].to(device, dtype= torch.int32)
-
-        melspecs = torch.transpose(melspecs, -1, -2) ## Changing to (batch, channel, time, n_mels) from (batch, channel, n_mels, time)
-
-        break
+    melspecs = torch.transpose(melspecs, -1, -2) ## Changing to (batch, channel, time, n_mels) from (batch, channel, n_mels, time)
     
     return summary(model, 
                    input_data = [melspecs, melspecs_lengths],
@@ -36,7 +37,7 @@ def get_summary(model: torch.nn.Module, dataloader = None, data = None) -> str:
                    mode = 'eval', 
                    verbose = 0)
 
-def get_writer(base_log_dir: str, comment = None) -> torch.utils.tensorboard.writer.SummaryWriter:
+def get_writer(base_log_dir: str, comment = None) -> SummaryWriter:
     base_log_dir = 'logs/'
 
     start_time = datetime.now()
