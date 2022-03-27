c475fe4794544ff571b285240978deb0e1dce2ab
Ashim Mahara <katyansun@gmail.com>
2022-03-27 12:03:07 +0545

    Added basic code for training tokenizers. Might need raise error from get_tokenizer.

--- utils/tokenizer.py
+++ utils/tokenizer.py
@@ -1,9 +1,51 @@
-from typing import Tuple
-
+from typing import List, Tuple, Union, Optional
 from transformers import PreTrainedTokenizerFast
 
+from tokenizers import Tokenizer
+from tokenizers.models import BPE
+from tokenizers.trainers import BpeTrainer
+from tokenizers.processors import TemplateProcessing
+from tokenizers.pre_tokenizers import Whitespace, Digits, Sequence
 
-def get_tokenizer(tokenizer_file_path: str) -> Tuple[PreTrainedTokenizerFast, str]:
+import pandas as pd
+from pandas import Series
+
+
+def train_tokenizer(tokenizer_file_path: str, 
+                    text: Union[Series, List[str]], 
+                    special_tokens: Optional[List[str]], 
+                    vocab_size: Optional[int] = 1000, 
+                    min_frequency: Optional[int] = 5) -> PreTrainedTokenizerFast:
+    
+    """Trains a Tokenizer on given text.
+    
+    Args:
+        tokenizer_file_path (str): path for saving the tokenizer.
+
+    Returns:
+        PreTrainedTokenizerFast: an instance of a HuggingFace PreTrainedTokenizer
+    """
+    
+    special_tokens= ["[BLANK]", "[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"]
+    tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
+    trainer = BpeTrainer(vocab_size = 1000, min_frequency = 5, special_tokens = special_tokens)
+
+    tokenizer.pre_tokenizer = Sequence([Whitespace(), Digits(individual_digits=True)])
+    
+    tokenizer.post_processor = TemplateProcessing(single="[BOS] $A [EOS]",
+                                                  special_tokens=[
+                                                      ("[BOS]", tokenizer.token_to_id("[BOS]")),
+                                                      ("[EOS]", tokenizer.token_to_id("[EOS]")),
+                                                      ]
+                                                  )
+    
+    tokenizer.train_from_iterator(iterator = text, trainer=trainer)
+    
+    tokenizer.save(tokenizer_file_path)
+    
+    return tokenizer
+
+def get_tokenizer(tokenizer_file_path: str) -> PreTrainedTokenizerFast:
     
     """
     Load a custom pretrained tokenizer. 
@@ -12,21 +54,19 @@ def get_tokenizer(tokenizer_file_path: str) -> Tuple[PreTrainedTokenizerFast, st
         File path for a pretrained huggingface tokenizer.
     
     Returns:
-        Huggingface PreTrainedTokenizerFast tokenizer object and blank token for CTC loss.
-    
+        Huggingface PreTrainedTokenizerFast tokenizer object.
     """
-    try: ##Check if tokenizer is defined
+    
+    try: ##Check if tokenizer is ~defined~ saved else train and save
         tokenizer
     except NameError as e: ## If tokenizer is not defined then initialize it
         tokenizer = PreTrainedTokenizerFast(tokenizer_file=tokenizer_file_path)
-        
     finally:
-        special_tokens_dict = {'pad_token': '[PAD]',
-                           'sep_token': '[SEP]',
-                           'mask_token': '[MASK]'}
+        special_tokens_dict = {'bos_token': '[BOS]',
+                               'pad_token': '[PAD]',
+                               'sep_token': '[SEP]',
+                               'mask_token': '[MASK]'}
 
         tokenizer.add_special_tokens(special_tokens_dict)
-
-        blank_token = "[BLANK]"
     
-    return tokenizer, blank_token
+    return tokenizer
