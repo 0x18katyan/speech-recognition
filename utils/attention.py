818aa7de70442415c33554b1a292c47b11813dc6
Ashim Mahara <katyansun@gmail.com>
2022-03-30 13:44:14 +0545

    Renamed Variables, Added Dot Product Attention

--- utils/attention.py
+++ utils/attention.py
@@ -1,3 +1,4 @@
+from json import decoder
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -8,7 +9,7 @@ class BadhanauAttention(nn.Module):
     
     """
     
-    def __init__(self, encoder_hidden_dim: int, decoder_hidden_dim: int, attention_dim: int):
+    def __init__(self, encoder_hidden_size: int, decoder_hidden_size: int, attention_dim: int):
         """
 
         Args:
@@ -19,13 +20,13 @@ class BadhanauAttention(nn.Module):
         
         super(BadhanauAttention, self).__init__()
 
-        self.U_a = nn.Linear(in_features = encoder_hidden_dim, out_features = attention_dim, bias = False)
+        self.U_a = nn.Linear(in_features = encoder_hidden_size, out_features = attention_dim, bias = False)
 
-        self.W_a = nn.Linear(in_features = decoder_hidden_dim, out_features = attention_dim, bias = False)
+        self.W_a = nn.Linear(in_features = decoder_hidden_size, out_features = attention_dim, bias = False)
                 
         self.v_t = nn.Linear(in_features = attention_dim, out_features = 1, bias = False)
         
-        self.ffn = nn.Linear(in_features = encoder_hidden_dim + decoder_hidden_dim, out_features = decoder_hidden_dim, bias = False)
+        self.ffn = nn.Linear(in_features = encoder_hidden_size + decoder_hidden_size, out_features = decoder_hidden_size, bias = False)
 
         self.tanh_layer = nn.Tanh()
         
@@ -40,32 +41,22 @@ class BadhanauAttention(nn.Module):
             torch.Tensor: the alignment score of the encoder_hidden_states.
         """
         
-        ##score is the e_ij
-        
-        # print(f"encoder_hidden_states.shape: {encoder_hidden_states.shape}")
-        
-        # print(f"decoder_hidden_state.shape: {decoder_hidden_state.shape}")
+        print(f"""encoder_hidden_states.shape: {encoder_hidden_states.shape},
+              decoder_hidden_state.shape: {decoder_hidden_state.shape}""")
+                
         
         U_a_out = self.U_a(encoder_hidden_states)        
         W_a_out = self.W_a(decoder_hidden_state)
-                
+        
+        ## e_ij is also called energy?    
         e_ij = self.tanh_layer(W_a_out + U_a_out)
-                
+
         e_ij = self.v_t(e_ij)
 
         ##Get the alignment scores for encoder_outputs and t-1 decoder_hidden_state
         alignment_scores = F.softmax(e_ij, dim = 1)
         
-        ##Apply the alignment scores to the encoder_hidden_states and calculate the weighted average
-        ##context_vector shape == [batch_size, 1 , encoder_hidden_states]
-        
-        context_vector = torch.bmm(alignment_scores.permute(0, 2, 1), encoder_hidden_states) ## context vector is how luong defines this
-        
-        # print(f"context_vector.shape: {context_vector.shape}, decoder_hidden_state.shape: {decoder_hidden_state.shape}")
-        
-        output = self.tanh_layer(self.ffn(torch.concat([decoder_hidden_state, context_vector], dim = -1)))
-
-        return output, context_vector
+        return alignment_scores
     
 ##source: https://github.com/bentrevett/pytorch-seq2seq, used for validation
     
@@ -101,3 +92,14 @@ class Attention(nn.Module):
         #attention= [batch size, src len]
         
         return F.softmax(attention, dim=1)
+    
+    
+class DotProductAttention(nn.Module):
+    def __init__(self, enc_hid_dim, dec_hid_dim):
+        super().__init__()
+        
+    def forward(self, encoder_outputs, hidden):
+        
+        scores = torch.bmm(encoder_outputs, hidden.permute(0, 2, 1))
+        
+        return F.softmax(scores, dim=1)
